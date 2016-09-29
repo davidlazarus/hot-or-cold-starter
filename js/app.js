c46e4298c6cf24019c0bcc02f9b18991eb5654d4
davidlazarus <davidlazarus89@gmail.com>
2016-09-29 15:31:52 +0200

    10

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ function game() {
 
          	else {
             console.log("guess, target, previousDistance, distance" + guess, target, previousDistance, distance);
-            if ( previousGuess === 'NaN') {
+            if ( previousGuess === NaN) {
                 if (guess > target) {
                     $('h2#feedback').html('Guess lower! Last guess: ' + guess);
                 } else if (guess < target) {
@@ -102,7 +102,7 @@ function game() {
   }
 
 function myPush(array, val) {
-  	array.push(val);
+  	array.push(val + ",");
   	return array;
   }
 
